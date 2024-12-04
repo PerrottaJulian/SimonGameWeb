04f3524220748734bfe693a1459657db8d98a187
PerrottaJulian <111912746+PerrottaJulian@users.noreply.github.com>
2024-12-04 11:28:57 -0300

    Starting proyect

--- SimonGame/src/app/app-component/app.component.ts
+++ SimonGame/src/app/app-component/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import bootstrap from '../../main.server';
+import { SimonComponent } from '../simon/simon.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SimonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ SimonGame/src/app/simon/simon.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'simon',
+  imports: [],
+  templateUrl: './simon.component.html',
+  styleUrl: './simon.component.scss'
+})
+export class SimonComponent implements OnInit {
+  title:string = "HOLA SOY SIMON"
+
+  ngOnInit(){
+    
+  }
+
+
+
+}
